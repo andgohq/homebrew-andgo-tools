5d2090fce4cfc490f8439af7ad3578edccb87d03
Toshihide Hara <toshihidehr@gmail.com>
2025-03-20 17:09:15 +0900

    bump

--- Formula/diet-mov.rb
+++ Formula/diet-mov.rb
@@ -2,9 +2,9 @@ class DietMov < Formula
   version "1.1.3"
   desc "Tools for small things"
   homepage ""
-  url "https://github.com/andgohq/homebrew-tools/archive/refs/tags/v1.1.2.tar.gz"
+  url "https://github.com/andgohq/homebrew-tools/archive/refs/tags/v1.1.3.tar.gz"
   # shasum -a 256 $FILE
-  sha256 "b7139364346d086f4e520627884282a78f55dead31a5dbfaaca92f45586f8eee"
+  sha256 "557ef01e7967455bf3dd886fba91731abe4c2ced4f7fd53142482f9f8ed6847a"
   license "MIT"
 
   depends_on "ffmpeg"
